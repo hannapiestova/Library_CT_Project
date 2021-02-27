5a7e5e92687031480c06b4706d253c247284c52e
Stitts5401 <jadenrs10@gmail.com>
2021-02-27 02:46:21 -0500

    Update CukesRunner.java
    Parellel test Jenkins/Grid

--- src/test/java/com/libraryAutomation/runners/CukesRunner.java
+++ src/test/java/com/libraryAutomation/runners/CukesRunner.java
@@ -17,7 +17,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/libraryAutomation/step_definitions",
         dryRun = false,
-        tags = "@logout",
+        tags = "",
         monochrome = true
 
 )
